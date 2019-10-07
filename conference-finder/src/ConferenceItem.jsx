e4a6c59a025f33049885b1bbeec9b5dd7bb81030
Sophia <montesbolahs@elsevier.com>
2019-10-07 13:51:28 +0100

    smb:GP19-37 conference item created

---
+++ conference-finder/src/ConferenceItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ConferenceItem = ({ conference }) => {
+    return (
+        <li>
+            <div class="card">
+                <div class="container">
+
+
+                </div>
+            </div>
+        </li>
+    )
+}
