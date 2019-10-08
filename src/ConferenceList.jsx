6d7e91060da9b7cef02b568e1b715177c292df9b
Sophia <montesbolahs@elsevier.com>
2019-10-08 13:50:04 +0100

    smb: GP19-34 removed redundant return from conferenceList, changed class name to camelcase and added it to ul instead of in div

--- src/ConferenceList.jsx
+++ src/ConferenceList.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ConferenceItem from "./ConferenceItem";
 import conferences from "./conferenceDataMock.json";
 
-const ConferenceList = () => {
-  return (
-    <div className="conference list">
-      <ul>
-        {conferences.map(conference => (
-          <ConferenceItem key={conference.id} conference={conference} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+const ConferenceList = () => (
+  <ul className="conferenceList">
+    {conferences.map(conference => (
+      <ConferenceItem key={conference.id} conference={conference} />
+    ))}
+  </ul>
+);
 
 export default ConferenceList;
