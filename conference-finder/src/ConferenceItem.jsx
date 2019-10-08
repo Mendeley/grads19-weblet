a1be900b97c0d75fdef0ab37908e837d952a3868
MerrettM <m.merrett@elsevier.com>
2019-10-08 10:51:44 +0100

    MM:GP19-37 removed residual create-react-app css in App.css, and made date and time more readable in ConferenceItem.jsx

--- conference-finder/src/ConferenceItem.jsx
+++ conference-finder/src/ConferenceItem.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 const ConferenceItem = ({ conference }) => {
   const { name, topic, dateTime, city } = conference;
+  const date = new Date(dateTime);
   return (
     <li>
       <div className="card">
         <h3>{name}</h3>
         <p>{topic}</p>
-        <p>{dateTime}</p>
+        <p>
+          {date.getDate()}/{date.getMonth() + 1}/{date.getFullYear()}
+        </p>
+        <p>
+          {date.getHours()}:{date.getMinutes()}
+        </p>
         <p>{city}</p>
       </div>
     </li>
