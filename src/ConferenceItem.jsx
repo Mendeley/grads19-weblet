9c7eabb8fb0e701ee1998330ce8fed512ea5f05a
Sophia <montesbolahs@elsevier.com>
2019-10-08 15:36:01 +0100

    Reverted from moment to .get methods

--- src/ConferenceItem.jsx
+++ src/ConferenceItem.jsx
@@ -3,15 +3,18 @@ import moment from "moment";
 
 const ConferenceItem = ({ conference }) => {
   const { name, topic, dateTime, city } = conference;
-  const date = moment(dateTime).format("DD/MM/YYYY");
-  const time = moment(dateTime).format("HH:mm");
+  const date = new Date(dateTime);
   return (
     <li>
       <div className="card">
         <h3>{name}</h3>
         <p>{topic}</p>
-        <p>{date}</p>
-        <p>{time}</p>
+        <p>
+          {date.getDate()}/{date.getMonth() + 1}/{date.getFullYear()}
+        </p>
+        <p>
+          {date.getHours()}:{date.getMinutes()}
+        </p>
         <p>{city}</p>
       </div>
     </li>
