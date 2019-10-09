13ade1a8edb1de9359a8faf39b9e61b93bfecb30
Sophia <montesbolahs@elsevier.com>
2019-10-09 16:47:23 +0100

    GP19-36 implemented loading screen and axios call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ConferenceList from "./ConferenceList";
 import axios from "axios";
-import conferences from "./conferenceDataMock.json";
+
+const getConferenceList = async () => {
+  try {
+    const response = await axios.get("../conferenceDataMock.json");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 function App() {
-  const getConferenceList = async () => {
-    try {
-      const response = await axios.get(
-        `jdbc:mysql://localhost:3306/conferenceFinder/conferences`
-      );
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [conferences, setConferences] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+
+      const conferences = await getConferenceList();
+      setConferences(conferences);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+  getConferenceList();
   return (
     <div className="App">
-      <ConferenceList conferences={conferences} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ConferenceList conferences={conferences} />
+      )}
     </div>
   );
 }
