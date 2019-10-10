ec6ca1548c45f739dcd8cba383dc262d7eaed16a
MerrettM <m.merrett@elsevier.com>
2019-10-10 11:12:25 +0100

    GRADS-0 spaces out functions

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 export const getTimestring = date => `${date.getHours()}:${date.getMinutes()}`;
+
 export const getDatestring = date =>
   `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
 export const getBackgroundColor = date => {
   const hours = date.getHours();
   if (hours >= 18 || hours < 6) {
