7adebaee1c759c27108a0afd54234f6cb599fa33
MerrettM <m.merrett@elsevier.com>
2019-10-14 13:58:29 +0100

    GP19-36: adds http://

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getConferenceList = async () => {
   try {
-    const response = await axios.get("localhost:8080/conferences");
+    const response = await axios.get("http://localhost:8080/conferences");
     return response.data;
   } catch (error) {
     console.log(error);
