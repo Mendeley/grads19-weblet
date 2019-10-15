0778d4d1b348e5a6160e2f7ab30cecfe18f6a8a7
karamkapoor <k.kapoor@elsevier.com>
2019-10-15 11:53:37 +0100

    GP19-42: explicitly states onClick

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+export const StyledButton = styled.button`
   text-align: center;
   background: lightgrey;
 `;
 
-const Button = ({ children, ...props }) => {
-    return (<StyledButton {...props}>
-        {children}
-    </StyledButton>);
+const Button = ({ children, onClick, ...props }) => {
+  return (
+    <StyledButton onClick={onClick} {...props}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
