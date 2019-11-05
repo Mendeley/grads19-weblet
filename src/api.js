3fef8d7bba162b282d2be481e9ad7682b4056033
Karen <k.zamani-pickford@elsevier.com>
2019-11-05 12:26:22 +0000

    GP19-61 add patch axios

--- src/api.js
+++ src/api.js
@@ -32,3 +32,13 @@ export const createNewConference = async conference => {
     throw error;
   }
 };
+
+export const updateConferenceById = async id => {
+  try {
+    const response = await axios.patch(`http://localhost:8080/conferences/${id}`);
+    return response.status;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
