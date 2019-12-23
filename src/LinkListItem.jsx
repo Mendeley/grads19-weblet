02ae3055352f7c8fe795483e645cb49371901b33
Sophia <montesbolahs@elsevier.com>
2019-12-23 15:23:50 +0000

    GP19-123: add linkListItem styling changes

--- src/LinkListItem.jsx
+++ src/LinkListItem.jsx
@@ -3,12 +3,16 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const StyledListItem = styled.li`
-  padding: 5px;
+  :hover {
+    background-color: papayawhip;
+    cursor: pointer;
+    display: inline;
+  }
 `;
 const StyledLink = styled(Link)`
   display: inline-block;
   text-decoration: none;
-  color: #7a517d;
+  color: #322d38;
 `;
 
 const LinkListItem = ({ displayText, link, className }) => {
