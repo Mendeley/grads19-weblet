dcb3a8ec42f4cb613ea15ba272ac1da66691a46b
Karen <k.zamani-pickford@elsevier.com>
2019-12-23 17:30:05 +0000

    GP19-133 check url not empty

--- src/Webscrape/WebscrapePage.jsx
+++ src/Webscrape/WebscrapePage.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import Input from "../Input";
 import { submitNewURL } from "../api";
-import { useHistory } from "react-router-dom";
 import { withCookies } from "react-cookie";
 import { toast } from "react-toastify";
 
 const WebscrapePage = ({ allCookies = {} }) => {
-	let history = useHistory();
 	const token = allCookies.sessionToken ? allCookies.sessionToken.token : null;
 	const [URL, setURL] = useState("");
 
@@ -15,18 +13,15 @@ const WebscrapePage = ({ allCookies = {} }) => {
 	};
 
 	const submitURL = async () => {
-		const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 		if (URL === "") {
 			toast.error("Please enter a URL");
-		} else if (regexp.test(URL)) {
+		} else {
 			try {
 				await submitNewURL(URL, token);
-				history.push("/add");
+// add setConference function with details from web scrape
 			} catch (error) {
 				console.log(error);
 			}
-		} else {
-			toast.info("URL not valid");
 		}
 	};
 
