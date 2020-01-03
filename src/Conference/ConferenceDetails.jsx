5a87ae1b3705481798952f7f54d142ac5b69be8e
Karen <k.zamani-pickford@elsevier.com>
2020-01-03 16:06:48 +0000

    GP19-144 format with prettier' '

--- src/Conference/ConferenceDetails.jsx
+++ src/Conference/ConferenceDetails.jsx
@@ -4,121 +4,120 @@ import { Link, useHistory } from "react-router-dom";
 import { withCookies } from "react-cookie";
 import Button from "../Button";
 import { getTimestring, getDatestring } from "../utils";
-import { deleteConferenceById, addFavouriteConference, removeFavouriteConference, getFavouritedConferencesByUserId } from "../api.js";
 import {
-  StyledCardHeading,
-  StyledForm,
-  StyledCard
+	deleteConferenceById,
+	addFavouriteConference,
+	removeFavouriteConference,
+	getFavouritedConferencesByUserId
+} from "../api.js";
+import {
+	StyledCardHeading,
+	StyledForm,
+	StyledCard
 } from "../StyledFormComponents";
 
 export const StyledLink = styled(Link)`
-  color: #7a517d;
+	color: #7a517d;
 `;
 
 export const StyledDescription = styled.p`
-  padding: 0 100px 50px 100px;
+	padding: 0 100px 50px 100px;
 `;
 
 export const ConferenceDetails = ({
-  conference,
-  id,
-  isLoading,
-  error,
-  allCookies = {}
+	conference,
+	id,
+	isLoading,
+	error,
+	allCookies = {}
 }) => {
-  const history = useHistory();
+	const history = useHistory();
 
-  const [userInterested, setUserInterested] = useState(false);
-  const conferenceDoesntExistInFavList = async () => {
-    try {
-      const favConferenceList = await getFavouritedConferencesByUserId(token);
-      var i;
-      for (i = 0; i < favConferenceList.length; i++) {
-        if (favConferenceList[i].id === conferenceId) {
-          setUserInterested(true);
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const [userInterested, setUserInterested] = useState(false);
+	const conferenceDoesntExistInFavList = async () => {
+		try {
+			const favConferenceList = await getFavouritedConferencesByUserId(token);
+			var i;
+			for (i = 0; i < favConferenceList.length; i++) {
+				if (favConferenceList[i].id === conferenceId) {
+					setUserInterested(true);
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  useEffect(() => {
-    conferenceDoesntExistInFavList();
-  }, [])
+	useEffect(() => {
+		conferenceDoesntExistInFavList();
+	}, []);
 
-  const token = allCookies.sessionToken ? allCookies.sessionToken.token : null;
-  const userId = allCookies.sessionToken
-    ? allCookies.sessionToken.userId
-    : null;
+	const token = allCookies.sessionToken ? allCookies.sessionToken.token : null;
+	const userId = allCookies.sessionToken ? allCookies.sessionToken.userId : null;
 
-  const conferenceId = parseInt(id);
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+	const conferenceId = parseInt(id);
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
 
-  if (error) {
-    return <p>An error has occurred...</p>;
-  }
+	if (error) {
+		return <p>An error has occurred...</p>;
+	}
 
-  const { name, topic, dateTime, city, description } = conference || {};
-  const date = new Date(dateTime);
+	const { name, topic, dateTime, city, description } = conference || {};
+	const date = new Date(dateTime);
 
-  const deleteConference = async id => {
-    try {
-      await deleteConferenceById(id, token);
-      history.push("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const deleteThisConference = () => {
-    deleteConference(id);
-  };
+	const deleteConference = async id => {
+		try {
+			await deleteConferenceById(id, token);
+			history.push("/");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const deleteThisConference = () => {
+		deleteConference(id);
+	};
 
-  const expressInterest = () => {
-    addFavouriteConference(userId, conferenceId, token);
-    setUserInterested(true);
-  };
+	const expressInterest = () => {
+		addFavouriteConference(userId, conferenceId, token);
+		setUserInterested(true);
+	};
 
-  const deexpressInterest = () => {
-    removeFavouriteConference(conferenceId, token);
-    setUserInterested(false);
-  };
+	const deexpressInterest = () => {
+		removeFavouriteConference(conferenceId, token);
+		setUserInterested(false);
+	};
 
-  return (
-    <StyledForm>
-      <StyledCard>
-        <StyledCardHeading className="name">{name}</StyledCardHeading>
-        {token && (
-          <StyledLink className="editLink" to={`/${id}/edit`}>
-            Edit Conference
-          </StyledLink>
-        )}
-        <p className="topic">{topic}</p>
-        <p className="date">{getDatestring(date)}</p>
-        <p className="time">{getTimestring(date)}</p>
-        <p className="city">{city}</p>
-        <StyledDescription className="description">
-          {description}
-        </StyledDescription>
-        {token && (
-          <>
-            <Button className="deleteButton" onClick={deleteThisConference}>
-              Delete Conference
-            </Button>
-            {!userInterested ? (
-              <Button onClick={expressInterest}>Express Interest</Button>
-            ) : (
-                <Button onClick={deexpressInterest}>Unexpress Interest</Button>)}
-
-
-
-          </>
-        )}
-      </StyledCard>
-    </StyledForm>
-  );
+	return (
+		<StyledForm>
+			<StyledCard>
+				<StyledCardHeading className="name">{name}</StyledCardHeading>
+				{token && (
+					<StyledLink className="editLink" to={`/${id}/edit`}>
+						Edit Conference
+					</StyledLink>
+				)}
+				<p className="topic">{topic}</p>
+				<p className="date">{getDatestring(date)}</p>
+				<p className="time">{getTimestring(date)}</p>
+				<p className="city">{city}</p>
+				<StyledDescription className="description">{description}</StyledDescription>
+				{token && (
+					<>
+						<Button className="deleteButton" onClick={deleteThisConference}>
+							Delete Conference
+						</Button>
+						{!userInterested ? (
+							<Button onClick={expressInterest}>Express Interest</Button>
+						) : (
+							<Button onClick={deexpressInterest}>Unexpress Interest</Button>
+						)}
+					</>
+				)}
+			</StyledCard>
+		</StyledForm>
+	);
 };
 
 export default withCookies(ConferenceDetails);
